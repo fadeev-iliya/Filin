61c32274a714c01465667fe7b2b391ea5f575454
fadeev-iliya <fadeev.iliya.2008@gmail.com>
2024-12-26 10:05:33 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Adafruit_GFX.h> // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>
+ 
+// These pins will also work for the 1.8" TFT shield
+ 
+//ESP32-WROOM
+#define TFT_DC 12 //A0
+#define TFT_CS 13 //CS
+#define TFT_MOSI 14 //SDA
+#define TFT_CLK 27 //SCK
+#define TFT_RST 0 
+#define TFT_MISO 0
+ 
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST);
+
